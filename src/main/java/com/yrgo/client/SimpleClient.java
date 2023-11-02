698cccbe55721ee268bf6fb4cc69943719378fe1
Tilda Kampman <tilda.kampman@gmail.com>
2023-11-02 09:44:02 +0100

    remove unused code that is not needed for assignement and close container

--- src/main/java/com/yrgo/client/SimpleClient.java
+++ src/main/java/com/yrgo/client/SimpleClient.java
@@ -20,55 +20,6 @@ public class SimpleClient {
         for(Customer customer:customers) {
             System.out.println(customer);
         }
-        //ADD NEW CUSTOMER
-        Customer testCustomer = new Customer("TEST", "COMPANYTEST", "TESTNOTES");
-        service.newCustomer(testCustomer);
-        //PRINT ALL CUSTOMERS
-        System.out.println("------");
-        System.out.println("UPDATED ALL CUSTOMERS:");
-        customers = service.getAllCustomers();
-        for(Customer customer:customers) {
-            System.out.println(customer);
-        }
-        //FIND SPECIFIC CUSTOMER
-        System.out.println("------");
-        System.out.println("FIND CUSTOMER BY ID:");
-        try{
-            System.out.println(service.findCustomerById("TEST"));
-        }catch(CustomerNotFoundException e){
-            e.getMessage();
-        }
-        //DELETE CUSTOMER
-        System.out.println("------");
-        System.out.println("DELETE CUSTOMER:");
-        service.deleteCustomer(customers.get(0));
-        customers = service.getAllCustomers();
-        for(Customer customer:customers) {
-            System.out.println(customer);
-        }
-        System.out.println("------");
-        //GET ALL DETAILS FROM SPECIFIC CUSTOMER
-        System.out.println("DETAILS FROM SPECIFIC CUSTOMER:");
-        try{
-            System.out.println(service.getFullCustomerDetail("TEST"));
-        }
-        catch(CustomerNotFoundException e){
-            e.getMessage();
-        }
-        //UPDATE CUSTOMER THAT ALREADY EXISTS AND PRINT ALL
-        testCustomer.setCompanyName("UPDATEDTEST");
-        service.updateCustomer(testCustomer);
-        System.out.println("------");
-        System.out.println("UPDATED CUSTOMER:");
-        for(Customer customer:customers) {
-            System.out.println(customer);
-        }
-        //FIND CUSTOMERS WITH A SPECIFIC NAME
-        System.out.println("------");
-        System.out.println("FIND CUSTOMER BY NAME:");
-        List<Customer> customersByName = service.findCustomersByName("UPDATEDTEST");
-        for(Customer customer:customersByName) {
-            System.out.println(customer);
-        }
+        container.close();
     }
 }
